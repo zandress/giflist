e87d3cddd1ea5555f60bb9a08d71b0328d9197c3
Zach <74432623+zandress@users.noreply.github.com>
2023-01-10 13:45:18 -0700

    im a goofball

--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -11,7 +11,7 @@ export class RedditService {
         src: '',
         author: '',
         name: '',
-        permaLink: '',
+        permalink: '',
         title: '',
         thumbnail: '',
         comments: 0,
