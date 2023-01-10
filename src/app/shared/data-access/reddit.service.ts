337452104c757889addaa4e16dd77e5e00c644b9
Zach <74432623+zandress@users.noreply.github.com>
2023-01-10 14:51:01 -0700

    creating the http calls in the service

--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -1,21 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { catchError, EMPTY, map, of } from 'rxjs';
+import { RedditPost, RedditResponse } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RedditService {
+  constructor(private http: HttpClient) {}
+
   getGifs() {
-    return of([
-      {
-        src: '',
-        author: '',
-        name: '',
-        permalink: '',
-        title: '',
-        thumbnail: '',
-        comments: 0,
-      },
-    ]);
+    return this.http
+      .get<RedditResponse>(
+        `https://www.reddit.com/r/${subreddit}/hot/.json?limit=100`
+      )
+      .pipe(
+        // If there is an error, just return an empty observable
+        // This prevents the stream from breaking
+        catchError(() => EMPTY),
+
+        // Convert response into the gif format we need
+        map((res) => this.convertRedditPostsToGifs(res.data.children))
+      );
+  }
+
+  private convertRedditPostsToGifs(posts: RedditPost[]){
+    // TODO: Implement
+  }
+
+  private getBestSrcForGif(post: RedditPost) {
+    // TODO: Implement
   }
 }
