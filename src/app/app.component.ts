855e01a42977a55dd04ff0edf2b85855daad3b32
Zach <74432623+zandress@users.noreply.github.com>
2023-01-11 16:02:08 -0700

    importing storage modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import {
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +30,14 @@ export class AppComponent {}
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(),
+    IonicStorageModule.forRoot({
+      driverOrder: [
+        // eslint-disable-next-line no-underscore-dangle
+        CordovaSQLiteDriver._driver,
+        Drivers.IndexedDB,
+        Drivers.LocalStorage,
+      ],
+    }),
     RouterModule.forRoot(
       [
         {
