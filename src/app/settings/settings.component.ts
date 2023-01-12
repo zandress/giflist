f0b6a66900385bfad28813ec47e52eb39aa09e1a
Zach <74432623+zandress@users.noreply.github.com>
2023-01-12 12:31:31 -0700

    formatting

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,63 +1,63 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
-import { PopoverController } from "@ionic/angular";
-import { SettingsService } from "../shared/data-access/settings.service";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { PopoverController } from '@ionic/angular';
+import { SettingsService } from '../shared/data-access/settings.service';
 
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { IonicModule } from "@ionic/angular";
-import { Settings } from "../shared/interfaces";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Settings } from '../shared/interfaces';
 
 @Component({
-    selector: 'app-settings',
-    template: `
-        <ion-header>
-            <ion-toolbar color="light">
-                <ion-buttons slot="end">
-                    <ion-button (click)="popoverCtrl.dismiss()">
-                        <ion-icon slot="icon-only" name="close"></ion-icon>
-                    </ion-button>
-                </ion-buttons>
-            </ion-toolbar>
-        </ion-header>
+  selector: 'app-settings',
+  template: `
+    <ion-header>
+      <ion-toolbar color="light">
+        <ion-buttons slot="end">
+          <ion-button (click)="popoverCtrl.dismiss()">
+            <ion-icon slot="icon-only" name="close"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
 
-        <ion-content class="ion-padding"> </ion-content>
+    <ion-content class="ion-padding"> </ion-content>
+  `,
+  styles: [
+    `
+      :host {
+        height: 100%;
+      }
+
+      ion-segment {
+        --ion-background-color: #fff;
+      }
     `,
-    styles: [
-        `
-            :host {
-                height: 100%;
-            }
-
-            ion-segment {
-                --ion-background-color: #fff;
-            }
-        `,
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent {
-    settingsForm = this.fb.nonNullable.group<Settings>({
-        sort: 'hot',
-        perPage: 10,
-    });
+  settingsForm = this.fb.nonNullable.group<Settings>({
+    sort: 'hot',
+    perPage: 10,
+  });
 
-    constructor(
-        private fb: FormBuilder,
-        public settingsService: SettingsService,
-        public popoverCtrl: PopoverController
-    ) {}
+  constructor(
+    private fb: FormBuilder,
+    public settingsService: SettingsService,
+    public popoverCtrl: PopoverController
+  ) {}
 
-    handleSave() {
-        this.settingsService.save(this.settingsForm.getRawValue());
-        this.popoverCtrl.dismiss();
-    }
+  handleSave() {
+    this.settingsService.save(this.settingsForm.getRawValue());
+    this.popoverCtrl.dismiss();
+  }
 }
 
 @NgModule({
-    imports: [CommonModule, ReactiveFormsModule, IonicModule],
-    declarations: [SettingsComponent],
-    exports: [SettingsComponent],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule],
+  declarations: [SettingsComponent],
+  exports: [SettingsComponent],
 })
 export class SettingsComponentModule {}
