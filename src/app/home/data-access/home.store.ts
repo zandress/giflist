e3985f9586d6f2eb9fd1456a35df4263e396d176
Zach <74432623+zandress@users.noreply.github.com>
2023-01-17 13:46:01 -0700

    setting up component store by defining the state

--- src/app/home/data-access/home.store.ts
+++ src/app/home/data-access/home.store.ts
@@ -1,48 +1,20 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { BehaviorSubject, combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { RedditService } from '../../shared/data-access/reddit/reddit.service';
+import { ComponentStore } from '@ngrx/component-store'
+
+interface HomeState {
+  currentlyLoadingGifs: string[];
+  loadedGifs: string[];
+  settingsModalIsOpen: boolean;
+}
+
 
 @Injectable()
-export class HomeStore {
-  currentlyLoadingGifs$ = new BehaviorSubject<string[]>([]);
-  loadedGifs$ = new BehaviorSubject<string[]>([]);
-  settingsModalIsOpen$ = new BehaviorSubject<boolean>(false);
-  subredditFormControl = new FormControl('gifs');
-
-  // Combine the stream of gifs with the streams determining their loading status
-  gifs$ = combineLatest([
-    this.redditService.getGifs(this.subredditFormControl),
-    this.currentlyLoadingGifs$,
-    this.loadedGifs$,
-  ]).pipe(
-    map(([gifs, currentlyLoadingGifs, loadedGifs]) =>
-      gifs.map((gif) => ({
-        ...gif,
-        loading: currentlyLoadingGifs.includes(gif.permalink),
-        dataLoaded: loadedGifs.includes(gif.permalink),
-      }))
-    )
-  );
-
-  constructor(private redditService: RedditService) {}
-
-  setLoading(permalink: string) {
-    // Add the gifs permalink to the loading array
-    this.currentlyLoadingGifs$.next([
-      ...this.currentlyLoadingGifs$.value,
-      permalink,
-    ]);
-  }
-
-  setLoadingComplete(permalinkToComplete: string) {
-    this.loadedGifs$.next([...this.loadedGifs$.value, permalinkToComplete]);
-
-    this.currentlyLoadingGifs$.next([
-      ...this.currentlyLoadingGifs$.value.filter(
-        (permalink) => !this.loadedGifs$.value.includes(permalink)
-      ),
-    ]);
+export class HomeStore extends ComponentStore<HomeState> {
+  constructor() {
+    super({
+      currentlyLoadingGifs: [],
+      loadedGifs: [],
+      settingsModalIsOpen: false,
+    });
   }
 }
