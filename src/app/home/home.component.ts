7ef4cf01cd595bc302c4f15b431f17c2f3955e50
Zach <74432623+zandress@users.noreply.github.com>
2023-01-17 12:34:53 -0700

    removing unneeded imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { BehaviorSubject, combineLatest, map, startWith } from 'rxjs';
+import { combineLatest, map, startWith } from 'rxjs';
 import { RedditService } from '../shared/data-access/reddit/reddit.service';
 import { Gif } from '../shared/interfaces/gif';
 import { GifListComponentModule } from './ui/gif-list.component';
