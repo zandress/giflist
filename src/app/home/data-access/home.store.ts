d88d72533f98af1af91f556e0837f3e054064351
Zach <74432623+zandress@users.noreply.github.com>
2023-01-18 10:24:18 -0700

    moving streams and methods over to the component store

--- src/app/home/data-access/home.store.ts
+++ src/app/home/data-access/home.store.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store'
+import { FormControl } from '@angular/forms';
+import { ComponentStore } from '@ngrx/component-store';
+import { RedditService } from 'src/app/shared/data-access/reddit/reddit.service';
 
 interface HomeState {
   currentlyLoadingGifs: string[];
@@ -7,14 +9,52 @@ interface HomeState {
   settingsModalIsOpen: boolean;
 }
 
-
 @Injectable()
 export class HomeStore extends ComponentStore<HomeState> {
-  constructor() {
+  subredditFormControl = new FormControl('gifs');
+
+  currentlyLoadingGifs$ = this.select((state) => state.currentlyLoadingGifs);
+  loadedGifs$ = this.select((state) => state.loadedGifs);
+  settingsModalIsOpen$ = this.select((state) => state.settingsModalIsOpen);
+
+  gifs$ = this.select(
+    this.redditService.getGifs(this.subredditFormControl),
+    this.currentlyLoadingGifs$,
+    this.loadedGifs$,
+    (gifs, currentlyLoadingGifs, loadedGifs) =>
+      gifs.map((gif) => ({
+        ...gif,
+        loading: currentlyLoadingGifs.includes(gif.permalink),
+        dataLoaded: loadedGifs.includes(gif.permalink),
+      }))
+  );
+
+  setLoading = this.updater((state, permalink: string) => ({
+    ...state,
+    currentlyLoadingGifs: [...state.currentlyLoadingGifs, permalink],
+  }));
+
+  setLoadingComplete = this.updater((state, permalinkToComplete: string) => ({
+    ...state,
+    loadedGifs: [...state.loadedGifs, permalinkToComplete],
+    currentlyLoadingGifs: [
+      ...state.currentlyLoadingGifs.filter(
+        (permalink) => permalink !== permalinkToComplete
+      ),
+    ],
+  }));
+
+  constructor(private redditService: RedditService) {
     super({
       currentlyLoadingGifs: [],
       loadedGifs: [],
       settingsModalIsOpen: false,
+    });
+  }
+
+  setModalIsOpen(value: boolean) {
+    this.patchState({
+      settingsModalIsOpen: value,
     });
   }
 }
